d82aa922894f7a9e75c9c7c0411e07a402a6d249
ValentinGassend <valentin.gassend@gmail.com>
2022-03-28 18:03:35 +0200

    create custom header/footer

--- webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/footer.blade.php
+++ webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/footer.blade.php
@@ -1,5 +1,21 @@
 <footer class="content-info">
   <div class="container">
+   <!-- Swiper JS -->
+   <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+   <!-- Initialize Swiper -->
+   <script>
+     var swiper = new Swiper(".mySwiper", {
+       slidesPerView: 2,
+       spaceBetween: 360,
+       freeMode: true,
+       loop: true,
+       grabCursor:true,
+       mousewheel: {
+        invert: false,
+      },
+     });
+   </script>
     @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
--- webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/head.blade.php
+++ webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/head.blade.php
@@ -3,4 +3,40 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+    <title>
+      <?php bloginfo('name'); ?>
+    </title>
+      <?php
+      wp_head();
+      show_admin_bar(true);
+      $header = get_field("header", "option");
+      $riot = $header["logos"]["riot"];
+      $valorant = $header["logos"]["valorant"];
+      $nav = $header["list_page"];
+      $profil = $header["profil"];
+      ?>
+</head>
+<body>
+    <header class="header" >
+        <div class="header_logo">
+            <img src="<?= $riot["url"] ?>" alt=" image of <?= $riot["title"] ?>">
+            <p>|</p>
+            <img src="<?= $valorant["url"] ?>" alt=" image of <?= $valorant["title"] ?>">
+        </div>
+        <nav class="nav">
+
+          
+
+            <?php foreach ($nav as $key => $value) { ?>
+
+            <a class="nav-link"><?= $value["name"] ?></a>
+
+            <?php } ?>
+
+        </nav>
+        <div class="profil">
+            <img class="picture" src="<?= $profil["profil_picture"]["url"]?>" alt="">
+            <p><?= $profil["pseudo"]?></p>
+        </div>
+    </header>
 </head>
