b4fd9d0ce2e72ea8880b19320a155155a761e4a3
ValentinGassend <valentin.gassend@gmail.com>
2022-03-28 18:02:39 +0200

    custom post preview on archive page

--- webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/content.blade.php
+++ webdev-wordpress/wordpress/wp-content/themes/s4-webdev/resources/views/partials/content.blade.php
@@ -1,9 +1,31 @@
-<article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-    @include('partials/entry-meta')
-  </header>
-  <div class="entry-summary">
-    @php the_excerpt() @endphp
-  </div>
+<article class="swiper-slide" @php post_class() @endphp>
+  @php
+      //this is the right file
+  @endphp
+    <a href="<?php echo get_permalink(get_the_ID())?>">
+    <div class="agent item">
+      <div class="titles">
+        <h3>{!! get_field('origin') !!}</h3>
+        <h2>{!! get_the_title() !!}</h2>
+        
+      </div>
+      <div>
+        <div class="mask">
+          <img src="<?= get_field('illustration')['url'] ?>" alt="">
+        </div>
+    </div>
+    <div class="capacitys">
+      <?php 
+      if (!empty(get_field('indiv_capacity'))) {
+        $capacitys=get_field('indiv_capacity');
+        foreach ($capacitys as $key => $value) {
+      ?> 
+        <div class="capacity">
+          <img src="<?php echo($value['url'])?>" alt=""> </div>
+      <?php
+        } 
+      }
+      ?>
+    </div>
+</div>
 </article>
